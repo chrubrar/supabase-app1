147c9e66a263dd88ca3e8d41365077ce85f1bfa0
Chru Brar <chrubrar@gmail.com>
2023-06-30 22:03:37 -0500

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        Home Page
+        Home Page Next Js vercel
       </div>
     </main>
   )
